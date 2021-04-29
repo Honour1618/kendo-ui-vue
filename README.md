6ece47c09af92467cdd6e168e513347feb494e43
lesly.fang <lesly.fang@analyticservice.net>
2021-04-29 15:27:25 +0800

    table inedit menuColumn

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# kendu-ui-vue
-a project of kendo ui for vue
+# kendo-vue-temp
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <div class="btn">
+      <router-link to="/">Customizing the Filter Component</router-link>
+      <router-link to="/edit">in-edit</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.btn {
+  width: 40%;
+  margin: 20px auto;
+  margin-bottom: 20px;
+  display: flex;
+}
+a {
+  flex: 1;
+}
+</style>
